c28bcba19fb029eb9635bee6813950e827a5308e
abhinav3254 <abhinav3254@gmail.com>
2024-09-08 20:37:05 +0530

    Starting Setup & Repetition Time!

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:expense_tracker/expenses.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter App',
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter App'),
-      ),
-      body: Column(
-        /*
-        Positioning
-        // mainAxisAlignment goes top to bottom in column
-        mainAxisAlignment: MainAxisAlignment.center,
-        // crossAxisAlignment goes left to right in row
-        crossAxisAlignment: CrossAxisAlignment.center,
-        */
-        children: <Widget>[
-          Container(
-            width: double.infinity,
-            child: Card(
-              child: Text('CHART!'),
-              elevation: 5,
-            ),
-          ),
-          Card(
-            child: Text('LIST OF TRANSCATION!'),
-          ),
-        ],
-      ),
-    );
-  }
+  runApp(
+    const MaterialApp(
+      home: Expenses(),
+    ),
+  );
 }
